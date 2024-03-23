1a0c38f65e74d438480d54660f08ef95e1df1b81
Mohamed ElAzhary <mohamed.elazhary@vodafone.com>
2024-03-23 04:38:04 +0200

    refactor: Env variables
    - adding and setup the dotenv package.
    - creating the .env file to hold the env vars values.
    - replacing the hardcoded values by the configured Env vars.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const job = require('./src/cron');
 (function () {
     try {
--- src/cron.js
+++ src/cron.js
@@ -3,14 +3,16 @@ const { CronJob } = require('cron');
 const axios = require('axios');
 const db = require('./config/db');
 const AirQuality = require('./models/airQualityModel');
-const API_KEY = '6b441523-dd55-4aad-91b9-9e4594a5dc9a';
+const API_KEY = process.env.API_KEY;
+const IQAIR_API_URL = process.env.IQAIR_API_URL;
+const NEAREST_CITY_API = process.env.NEAREST_CITY_API;
 
 const job = new CronJob('*/1 * * * *', async () => {
     const latitude = 48.856613;
     const longitude = 2.352222;
 
     try {
-        const response = await axios.get(`http://api.airvisual.com/v2/nearest_city?lat=${latitude}&lon=${longitude}&key=${API_KEY}`);
+        const response = await axios.get(`${IQAIR_API_URL}${NEAREST_CITY_API}?lat=${latitude}&lon=${longitude}&key=${API_KEY}`);
 
         console.log(response?.data?.data);
         const airQualityData = response?.data?.data?.current?.pollution;
